f4974997390b699d6b558f7df3535adf3e8685f1
Yhoan Galeano <ygaleano@rapicredit.com>
2024-03-30 23:22:03 -0500

    create services in spring boot

---
+++ school/src/main/java/com/yhoangaleano/school/SchoolRepository.java
@@ -0,0 +1,6 @@
+package com.yhoangaleano.school;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Integer> {
+}
---
+++ school/src/main/java/com/yhoangaleano/school/Student.java
@@ -0,0 +1,14 @@
+package com.yhoangaleano.school;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private String firstname;
+    private String lastname;
+    private String email;
+}
